d4408d5ad11ed1f730cf6f0b75eac9cc048efc70
HimanshuBarak <himanshubarak11@gmail.com>
2021-09-19 02:27:52 +0530

    basic layout done

---
+++ frontend/my-app/src/components/Model.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './model.css'
+
+function Model() {
+    return (
+        <div className="model-container">
+            
+        </div>
+    )
+}
+
+export default Model
---
+++ frontend/my-app/src/components/Symptoms.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './symptoms.css'
+
+
+function Symptoms() {
+    return (
+        <div>
+            <div className="symptoms">
+
+            </div>
+           
+        </div>
+    )
+}
+
+export default Symptoms
