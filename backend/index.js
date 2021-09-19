329a9033c91682842022d0482c5cfa39819c9aa9
Bibi <Bibi@katies-mac.local>
2021-09-19 10:43:16 -0400

    post and get patient apis work

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,8 @@ app.use(cookieParser());
 app.use(express.json());
 
 const postPatientApi = "https://fhir.a4bb78it5rl2.static-test-account.isccloud.io/Observation";
+const getPatientApi = "https://fhir.a4bb78it5rl2.static-test-account.isccloud.io/Observation?identifier=mithack21";
+
 
 //FHIR json Observarion format
 let obj = {
@@ -73,7 +75,12 @@ let obj = {
             display: "test health data"
         }
         ]
-    }
+    },
+    identifier: [ 
+        {
+            value: "mithack21"
+        }
+    ]
 }
 
 //working API
@@ -87,8 +94,8 @@ app.post("/postPatient", async (req, res) => {
 
     //Reformat date YYYY-MM-DD
     obj.code.coding[0].display = new Date(mydate).toISOString().split('T')[0];
-
-    obj.code.text = req.body.date;
+    obj.code.text = req.body.todayDate;
+    obj.meta.tag[0].display = req.body.phaseResult;
     
     const response = await axios.post(postPatientApi,obj, {
         headers
@@ -102,22 +109,21 @@ app.get("/getPatientForResearch", async (req, res) => {
     const headers = {
         "x-api-key": "srOd0BhfKd5k7z5Dy1SUWaQpWfOg1ycH7ShG5R6D"
     }
-    const menstrual_id = "mithack_21";
-    const response = await axios.get(postPatientApi,{headers});
+    const response = await axios.get(getPatientApi, {headers});
+    
     console.log(JSON.stringify(response.data));
-    //console.log("length = ", response.data.entry.length);
 
-    const hackmitPatients = response.data.entry.filter((patient) => {
-        //console.log(JSON.stringify(patient))
-        if(isNaN(patient.resource.id)){
-            console.log(patient.resource);
-        }
-        return patient.resource.id == menstrual_id});
+    const hackmitPatients = response.data;
+    
+    // .filter((patient) => {
+    //     //console.log(JSON.stringify(patient))
+    //     if(isNaN(patient.resource.id)){
+    //         console.log(patient.resource);
+    //     }
+    //     return patient.resource.id == menstrual_id});
 
-    //ISSUE: query result changed during iteration. No results can be found bc of id changes
     res.json(hackmitPatients);
 });
-
 
 //To test this API, try sending this obj as a body. Tweak the day param to get different results
 const testBody = {
@@ -129,145 +135,156 @@ const testBody = {
 //Get the phase prediction using ML model
 app.post("/getPredictionResult", (req, res) => {
     console.log(req.body);
+    let msg = "";
 
     const offset = new Date(req.body.lastPeriodDate).getTimezoneOffset();
     let last_period_date = new Date(req.body.lastPeriodDate).getTime()-(offset*60*1000); //Date
-
     let length_of_cyle = parseInt(req.body.lengthCycle); //int
     let flow_level = req.body.flow; //string
 
-    // if (flow_level == "heavy") {
-    //     //update FirstDayofHigh and TotalNumberofHighDays
-    // }
-    let input = {
-        ClientID: "nfp8122",
-        CycleNumber: 3,
-        Group: 0,
-        CycleWithPeakorNot: 1,
-        ReproductiveCategory: 0,
-        LengthofCycle: 	length_of_cyle, //29,
-        MeanCycleLength: 27.33,	
-        // EstimatedDayofOvulation: 15,
-        LengthofLutealPhase: 14, 	
-        FirstDayofHigh: 13,	
-        TotalNumberofHighDays: 1,	
-        TotalHighPostPeak: 	0,
-        TotalNumberofPeakDays: 2,	
-        TotalDaysofFertility: 5,	
-        TotalFertilityFormula: 13,	
-        LengthofMenses: 5,	
-        MeanMensesLength: 4.49,	
-        MensesScoreDayOne: 3,	
-        MensesScoreDayTwo: 3,	
-        MensesScoreDayThree: 2,	
-        MensesScoreDayFour: 1,	
-        MensesScoreDayFive: 1,	
-        MensesScoreDaySix: 0,	
-        MensesScoreDaySeven: 0,	
-        MensesScoreDayEight: 0,	
-        MensesScoreDayNine: 0,	
-        MensesScoreDayTen: 0,	
-        MensesScoreDay11: 0,	
-        MensesScoreDay12: 0,	
-        MensesScoreDay13: 0,	
-        MensesScoreDay14: 0,	
-        MensesScoreDay15: 0,	
-        TotalMensesScore: 10,	
-        MeanBleedingIntensity: 0,	
-        NumberofDaysofIntercourse: 9.04,	
-        IntercourseInFertileWindow: 1,	
-        UnusualBleeding: 0,	
-        PhasesBleeding: 0,	
-        IntercourseDuringUnusBleed: 0,	
-        Age: 36,	
-        AgeM: 39,	
-        Maristatus: 0,	
-        MaristatusM: 0,	
-        Yearsmarried: 10,	
-        Wedding: 0,	
-        Religion: 0,	
-        ReligionM: 0,	
-        Ethnicity: 0,	
-        EthnicityM: 0,	
-        Schoolyears: 8,	
-        SchoolyearsM: 8,	
-        OccupationM: 3,	
-        IncomeM: 5,	
-        Height: 63,	
-        Weight: 120,	
-        Reprocate: 0,	
-        Numberpreg: 3,	
-        Livingkids: 3,	
-        Miscarriages: 0,	
-        Abortions: 0,	
-        Medvits: 0,	
-        Medvitexplain: 0,	
-        Gynosurgeries: 0,	
-        LivingkidsM: 0,	
-        Boys: 0,	
-        Girls: 0,	
-        MedvitsM: 0,	
-        MedvitexplainM: 0,	
-        Urosurgeries: 0,	
-        Breastfeeding: 0,	
-        Method: 9,	
-        Prevmethod: 0,	
-        Methoddate: 0,	
-        Whychart: 2,	
-        Nextpreg: 7,	
-        NextpregM: 7,	
-        Spousesame: 1,	
-        SpousesameM: 1,	
-        Timeattemptpreg: 0,	
-        BMI: 21.25472411,
-    }
-    //ML prediction of ovulation day
-    const output = model.predict(input);
-    
-    const ovulation_res = output["className"]; 
-    console.log(ovulation_res);
-
-    //Calculations in milisecond
-
-    let ovulation_milisec = ovulation_res * 24 * 60 * 60 * 1000;
-    let length_of_cyle_milisec = length_of_cyle * 24 * 60 * 60 * 1000;
-    let max_period_days = 7 * 24 * 60 * 60 * 1000; //7 days
-    let next_ovulation_date = last_period_date + ovulation_milisec;
-    let next_period_date = last_period_date + length_of_cyle_milisec;
-
-    const offset_today = new Date().getTimezoneOffset();
-    const today = new Date().getTime()-(offset*60*1000);;
-    
-    let msg = "";
-    //These are not all the possible cases but good enough for demo
-    //The furthest date of prediction is the next menstrual window
-
-    console.log("ovulation",next_ovulation_date);
-    console.log("today",today);
-    console.log("diff", today-next_ovulation_date);
-
-    //Ovulation
-    if (today == next_ovulation_date || Math.abs(today - next_ovulation_date) < 86400000){ //1 day
-        msg = "ovulation";
-    }
-    //Current menstrual
-    else if (today >= next_period_date && today <= next_period_date + max_period_days) {
-        msg = "menstrual";
-    }
-    //Next menstrual
-    else if (today >= last_period_date && today <= last_period_date + max_period_days) {
-        msg = "menstrual";
-    }
-    //Luteal phase
-    else if (today > next_ovulation_date && today < next_period_date) {
-        msg = "luteal";
-    }
-    //Follicular phase
-    else if (today >= last_period_date && today < next_ovulation_date) {
-        msg = "follicular";
+    if (flow_level == "heavy" || flow_level == "medium") {
+        msg = "menstrual"
     }
     else {
-        msg = "follicular"
+        const input = {
+            ClientID: "nfp8122",
+            CycleNumber: 3,
+            Group: 0,
+            CycleWithPeakorNot: 1,
+            ReproductiveCategory: 0,
+            LengthofCycle: 	length_of_cyle, //29,
+            MeanCycleLength: 27.33,	
+            // EstimatedDayofOvulation: 15,
+            LengthofLutealPhase: 14, 	
+            FirstDayofHigh: 13,	
+            TotalNumberofHighDays: 1,	
+            TotalHighPostPeak: 	0,
+            TotalNumberofPeakDays: 2,	
+            TotalDaysofFertility: 5,	
+            TotalFertilityFormula: 13,	
+            LengthofMenses: 5,	
+            MeanMensesLength: 4.49,	
+            MensesScoreDayOne: 3,	
+            MensesScoreDayTwo: 3,	
+            MensesScoreDayThree: 2,	
+            MensesScoreDayFour: 1,	
+            MensesScoreDayFive: 1,	
+            MensesScoreDaySix: 0,	
+            MensesScoreDaySeven: 0,	
+            MensesScoreDayEight: 0,	
+            MensesScoreDayNine: 0,	
+            MensesScoreDayTen: 0,	
+            MensesScoreDay11: 0,	
+            MensesScoreDay12: 0,	
+            MensesScoreDay13: 0,	
+            MensesScoreDay14: 0,	
+            MensesScoreDay15: 0,	
+            TotalMensesScore: 10,	
+            MeanBleedingIntensity: 0,	
+            NumberofDaysofIntercourse: 9.04,	
+            IntercourseInFertileWindow: 1,	
+            UnusualBleeding: 0,	
+            PhasesBleeding: 0,	
+            IntercourseDuringUnusBleed: 0,	
+            Age: 36,	
+            AgeM: 39,	
+            Maristatus: 0,	
+            MaristatusM: 0,	
+            Yearsmarried: 10,	
+            Wedding: 0,	
+            Religion: 0,	
+            ReligionM: 0,	
+            Ethnicity: 0,	
+            EthnicityM: 0,	
+            Schoolyears: 8,	
+            SchoolyearsM: 8,	
+            OccupationM: 3,	
+            IncomeM: 5,	
+            Height: 63,	
+            Weight: 120,	
+            Reprocate: 0,	
+            Numberpreg: 3,	
+            Livingkids: 3,	
+            Miscarriages: 0,	
+            Abortions: 0,	
+            Medvits: 0,	
+            Medvitexplain: 0,	
+            Gynosurgeries: 0,	
+            LivingkidsM: 0,	
+            Boys: 0,	
+            Girls: 0,	
+            MedvitsM: 0,	
+            MedvitexplainM: 0,	
+            Urosurgeries: 0,	
+            Breastfeeding: 0,	
+            Method: 9,	
+            Prevmethod: 0,	
+            Methoddate: 0,	
+            Whychart: 2,	
+            Nextpreg: 7,	
+            NextpregM: 7,	
+            Spousesame: 1,	
+            SpousesameM: 1,	
+            Timeattemptpreg: 0,	
+            BMI: 21.25472411,
+        }
+        //ML prediction of ovulation day
+        const output = model.predict(input);
+        
+        const ovulation_res = output["className"]; 
+        console.log(ovulation_res);
+
+        //Calculations in milisecond
+        let ovulation_milisec = ovulation_res * 24 * 60 * 60 * 1000;
+        let length_of_cyle_milisec = length_of_cyle * 24 * 60 * 60 * 1000;
+        let max_period_days = 7 * 24 * 60 * 60 * 1000; //7 days
+        let next_ovulation_date = last_period_date + ovulation_milisec;
+        let next_period_date = last_period_date + length_of_cyle_milisec;
+
+        const offset_today = new Date().getTimezoneOffset();
+        const today = new Date().getTime()-(offset*60*1000);;
+        
+        //These are not all the possible cases but good enough for demo
+        //The furthest date of prediction is the next menstrual window
+
+        console.log("ovulation",next_ovulation_date);
+        console.log("today",today);
+        console.log("diff", today-next_ovulation_date);
+
+        //Ovulation
+        if (today == next_ovulation_date || Math.abs(today - next_ovulation_date) < 86400000){ //1 day
+            msg = "ovulation";
+        }
+        //Current menstrual
+        else if (today >= next_period_date && today <= next_period_date + max_period_days) {
+            if (flow_level == "light") {
+                msg = "menstrual";
+                
+            }
+            else {
+                msg = "follicular";
+            }
+        }
+        //Next menstrual
+        else if (today >= last_period_date && today <= last_period_date + max_period_days) {
+            if (flow_level == "light") {
+                msg = "menstrual";
+            }
+            else {
+                msg = "follicular";
+            }
+        }
+        //Luteal phase
+        else if (today > next_ovulation_date && today < next_period_date) {
+            msg = "luteal";
+        }
+        //Follicular phase
+        else if (today >= last_period_date && today < next_ovulation_date) {
+            msg = "follicular";
+        }
+        else {
+            msg = "follicular"
+        }
     }
     res.json({ message: msg });
 });
